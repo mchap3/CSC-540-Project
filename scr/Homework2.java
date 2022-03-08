daa41cf94730cd63af1f37e6b56551dd4317edb6
cpatel3 <cpatel3@ncsu.edu>
2022-03-07 23:15:48 -0500

    adding HW2

--- scr/Homework2.java
+++ scr/Homework2.java
@@ -27,7 +27,7 @@ public class Homework2 {
 
 		initialize();
 
-		try {
+		try { 
 			boolean canAfford = checkAbilityToStudy("Todd");
 			// ************************************************************************
 
