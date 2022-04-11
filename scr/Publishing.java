d4615a107a8c3cc5d34c0c369b30cc8a5d90e682
mchapma6 <mchapma6@ncsu.edu>
2022-04-10 23:23:49 -0400

    update compiler

--- scr/Publishing.java
+++ scr/Publishing.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 
-//javac database_setup.java DBManager.java DBTablePrinter.java User.java Reports.java Distributors.java Production.java Publishers.java
+//javac database_setup.java DBManager.java DBTablePrinter.java User.java Reports.java Distribution.java Production.java Publishing.java Publisher.java DistributionTeam.java Editor.java FinancialTeam.java
 //java database_setup
 
 public class Publishing {
